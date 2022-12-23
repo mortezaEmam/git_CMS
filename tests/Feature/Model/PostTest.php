97a62745ce25f6b66dfbed9189ddad7716d9f521
morteza emamverdi <morteza.emamverdi1372@gmail.com>
2022-12-23 19:20:49 +0330

    modify class PostTest for relation posts with tags and modify Model post for this relation(posts,tags)

--- tests/Feature/Model/PostTest.php
+++ tests/Feature/Model/PostTest.php
@@ -4,6 +4,7 @@ namespace Tests\Feature\Model;
 
 use App\Models\Category;
 use App\Models\Post;
+use App\Models\Tag;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -30,4 +31,13 @@ class PostTest extends TestCase
         $this->assertTrue(isset($post->category->id));
         $this->assertTrue($post->category instanceof Category);
     }
+    public function test_Post_ralationship_with_tags()
+    {
+        $count = rand(1,10);
+        $post = Post::factory()->hasTags($count)->create();
+        $this->assertCount($count,$post->tags);
+        $this->assertTrue($post->tags->first() instanceof Tag);
+
+    }
+
 }
